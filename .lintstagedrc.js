da70594a4550a71ec021c7ff3e2f144dd4931e5c
Santana James <santana.james@gmail.com>
2021-10-08 12:28:00 -0400

    SJ-0000: add jira id using prepare-commit-msg hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-    'cartridges/**/cartridge/client/**/*.{js,json,css}': [
-        'eslint --fix',
-        'git add',
+    'cartridges/**/cartridge/client/**/*.{js,json}': [
+        'eslint --fix'
     ],
+    'cartridges/**/cartridge/client/**/*.scss': [
+        'stylelint --fix'
+    ],
+    'cartridges/**/cartridge/client/**/*.isml': [
+        'isml-lint --autofix'
+    ]
 };
