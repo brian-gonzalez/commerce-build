18bcc7ddd0842080869fd0bb7e30e8827e53ace2
Santana James <santana.james@gmail.com>
2021-11-04 00:33:36 -0400

    update build settings

--- .ismllintrc.js
+++ .ismllintrc.js
@@ -1,18 +1,12 @@
 module.exports = {
     'rootDir': './cartridges',
-    // TODO: Modify ignore:[] paths per project
     'ignore': [
-        // 'bm_',
-        // 'int_',
-        // 'lib_',
-        // 'plugin_',
         'app_storefront_base/',
         'bm_app_storefront_base/',
         'modules/',
     ],
     'rules': {
         'contextual-attrs': {},
-        // 'enforce-require': {},
         'leading-iscache': {},
         'leading-iscontent': {},
         'no-deprecated-attrs': {},
